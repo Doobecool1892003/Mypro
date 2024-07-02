407aa607127305b09de0270e19f8af69980c5226
Doobecool1892003 <hoang18903@gmail.com>
2024-07-02 12:33:54 +0700

    upload file

--- projects/lesson3/lesson3.ts
+++ projects/lesson3/lesson3.ts
@@ -1,11 +1,13 @@
 let pro: {
     name: string,
     age: number,
-    address: string
+    address: string,
+    phone: number
 }= {
     name: 'Hoang',
     age: 21,
-    address: 'hhh'
+    address: 'hhh',
+    phone: 11111222
 };
 
 // pro.home = 'Hanoi'
