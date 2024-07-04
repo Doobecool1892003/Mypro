4e764dc081a6d375b405de59bd02623708c28d16
Doobecool1892003 <hoang18903@gmail.com>
2024-07-04 18:16:21 +0700

    new code

--- projects/lesson4/lesson4.ts
+++ projects/lesson4/lesson4.ts
@@ -1,26 +1,4 @@
-type Htype = number | string | object | boolean;
-
-function addNumberOrString (a: Htype, b: Htype ){
-    if(typeof a === 'number' && typeof b === 'number'){
-        return a + b;
-    }
-    if(typeof a === 'string' && typeof b === 'string'){
-        return a.concat(b);
-    }
-    throw new Error('Parameters must be numbers or strings');
+let i = 0;
+for(; i < 10; i++) {
+    console.log(i);
 }
-
-console.log("check >>: ", addNumberOrString('Hoang dz', 'jss'));
-
-let discount: number;
-let itemCount = 11;
-
-if(itemCount > 0  && itemCount <= 5) {
-    discount = 5;
-}else if(itemCount > 5 && itemCount <= 10) {
-    discount = 10;
-}else {
-    discount = 15;
-}
-
-console.log(`You got ${discount}% discount.`);
