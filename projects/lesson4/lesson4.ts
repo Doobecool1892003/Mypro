6b4aeaca15ecb98d34872759ef52bde808489a8d
Doobecool1892003 <hoang18903@gmail.com>
2024-07-04 18:41:06 +0700

    new code

--- projects/lesson4/lesson4.ts
+++ projects/lesson4/lesson4.ts
@@ -1,4 +1,16 @@
-let i = 0;
-for(; i < 10; i++) {
-    console.log(i);
+let users = [
+    {name:'Hoang', age: 18, salary: 1400},
+    {name:'Huy',   age: 21, salary: 1500},
+    {name:'Hoa',   age: 19, salary: 1600},
+    {name:'Hoang', age: 30, salary: 10000},
+];
+
+let count = 0;
+
+for(var i = 0; i < users.length; i++){
+    if(users[i].salary > 0) {
+        count += users[i].salary;
+    }
 }
+
+console.log(count);
